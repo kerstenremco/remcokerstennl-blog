067e68f5c1fb7f4c23f43d12f0f36749ec8f31a5
Remco Kersten <kersten.remco@outlook.com>
2024-09-15 19:21:14 +0200

    Style fixes stepper

--- frontend/src/components/Stepper.jsx
+++ frontend/src/components/Stepper.jsx
@@ -1,8 +1,12 @@
 export default function Stepper(props) {
-    const headers = props.headers;
-    return (
-        <ul class="steps xl:steps-vertical w-full xl:sticky xl:top-0 bg-base-100" id="stepper">
-            {headers.map((header) => <li class="step">{header}</li>)}
-            </ul>
-    )
+  const headers = props.headers;
+  return (
+    <div className="hidden xl:block sticky top-0">
+      <ul class="steps steps-vertical w-full bg-base-100" id="stepper">
+        {headers.map((header) => (
+          <li class="step alingleft">{header}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
