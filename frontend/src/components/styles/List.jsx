21b93a130202b29329833b75007536548dcd554b
Remco Kersten <kersten.remco@outlook.com>
2024-09-15 20:41:48 +0200

    Small changes in styling

--- frontend/src/components/styles/List.jsx
+++ frontend/src/components/styles/List.jsx
@@ -1,4 +1,4 @@
 export default function List(props) {
     if(props.type == "ol") return <ol class="list-inside my-4 list-decimal">{props.children}</ol>
-    else return <ul class="list-inside my-4 list-disc">{props.children}</ul>
+    else return <ul class="list-inside my-4 list-image-[url(/emoji/point.png)]">{props.children}</ul>
 }
